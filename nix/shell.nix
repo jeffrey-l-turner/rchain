97f0f1285729050eff8962322f35ccc4f4ab6eba
Stephen Preston <stevepreston747@gmail.com>
2023-02-23 18:01:47 -0800

    nix.shell wip - need to configure correct package versions

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,6 +1,11 @@
-{ pkgs ? import <nixpkgs> {}}:
+with (import <nixpkgs> {});
 
-pkgs.mkShell {
+let 
+	jdk = pkgs.jdk11;
+	sbt = pkgs.sbt.override { jre = pkgs.jdk11; };
+	# jflex = pkgs.jflex
+
+in mkShell {
   buildInputs = with pkgs; [
     rustc
     cargo
@@ -10,7 +15,17 @@ pkgs.mkShell {
     ps
     bash
     which
+		haskellPackages.BNFC
+		git 
+		jflex-1.7.0
+		sbt 
+		jdk
   ];
+
+	shellHook = ''
+      export SBT_OPTS="-Xmx4g -Xss2m -Dsbt.supershell=false"
+			export PATH="/usr/local/opt/openjdk/bin:$PATH"
+  '';
 
   RUST_BACKTRACE = 1;
 }
