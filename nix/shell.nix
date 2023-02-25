75a4f35ea4120368a337d5b11f8c2f992ea86a63
Stephen Preston <stevepreston747@gmail.com>
2023-02-24 17:45:15 -0800

    working compile on mac

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,13 +1,9 @@
 with (import <nixpkgs> {});
 
-let 
-	pkgs = import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/fcc8660d359d2c582b0b148739a72cec476cfef5.tar.gz";
-    }) {};
-		
-	jdk = pkgs.jdk11;
-	sbt = pkgs.sbt.override { jre = pkgs.jdk11; };
-	jflex = pkgs.jflex;
+let
+    jdk = pkgs.jdk11;
+    sbt = pkgs.sbt.override { jre = pkgs.jdk11; };
+    jflex = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/fcc8660d359d2c582b0b148739a72cec476cfef5.tar.gz) { }; 
 
 in mkShell {
   buildInputs = with pkgs; [
@@ -22,14 +18,14 @@ in mkShell {
     which
     haskellPackages.BNFC
     git 
-    jflex
+    jflex.jflex
     sbt 
     jdk
   ];
 
-	shellHook = ''
+    shellHook = ''
       export SBT_OPTS="-Xmx4g -Xss2m -Dsbt.supershell=false"
-			export PATH="/usr/local/opt/openjdk/bin:$PATH"
+            export PATH="/usr/local/opt/openjdk/bin:$PATH"
   '';
 
   RUST_BACKTRACE = 1;
