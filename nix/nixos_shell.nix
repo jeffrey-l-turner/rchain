ffd21d7f4865ec93e9c05d9e8e297c404a1f4de7
admin <admin@composedaor.org>
2023-03-02 22:03:32 +0000

    restoring to original, try --gc nix-store

--- nix/nixos_shell.nix
+++ nix/nixos_shell.nix
@@ -1,14 +1,4 @@
-{ pkgs ? import <nixpkgs> {
-  config = {
-    packageOverrides = pkgs: {
-      sbt = pkgs.sbt.override { jre = pkgs.openjdk11; };
-      buildFHSUserEnv = pkgs.buildFHSUserEnv.override {
-        name = "akka-grpc";
-        targetPkgs = pkgs: [pkgs.sbt pkgs.glibc ];
-      };
-    };
-  };
-}}:
+{ pkgs ? import <nixpkgs> {} }:
 
 let 
   pkgs = import (builtins.fetchTarball {
@@ -16,11 +6,13 @@ let
     }) {};
 
   myPkg = pkgs.jflex;
+  sbt = pkgs.sbt.override { jre = pkgs.jdk11; };
+  # jflex = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/fcc8660d359d2c582b0b148739a72cec476cfef5.tar.gz) { };
 
 in (pkgs.buildFHSUserEnv {
   name = "rchain";
 
-  targetPkgs = pkgs: with pkgs; [ sbt glibc haskellPackages.BNFC git myPkg jdk11 which rustc cargo rustfmt zsh ];
+  targetPkgs = pkgs: with pkgs; [ sbt glibc haskellPackages.BNFC git myPkg jdk11 which ];
 
   profile = ''
     export SBT_OPTS="-Xmx4g -Xss2m -Dsbt.supershell=false"
