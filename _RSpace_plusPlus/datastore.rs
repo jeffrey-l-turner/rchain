43487e997da576eca822f9a7e9cd0925442c1aed
chumpp <noahjevans@gmail.com>
2023-03-03 01:01:48 -0800

    more changes to test

--- _RSpace_plusPlus/datastore.rs
+++ _RSpace_plusPlus/datastore.rs
@@ -9,97 +9,116 @@ use heed::Env;
 pub struct FileDatabase {
     pub name: String,
     pub age: u8,
+    pub environment: Env,
     pub db: Database<Str, Str>,
-    //pub env: Env,
 }
 
-// impl FileDatabase {
-//     pub fn new(name: String, age: u8) -> FileDatabase {
+impl FileDatabase {
+    pub fn new(name: String, age: u8) -> FileDatabase {
 
-//     let env_path = Path::new("target").join("test.mdb");
-//     let _ = fs::remove_dir_all(&env_path);
-//     fs::create_dir_all(&env_path);
-//     let env = EnvOpenOptions::new()
-//         .map_size(10 * 1024 * 1024) // 10MB
-//         .max_dbs(3)
-//         .open(env_path).unwrap();
-
-//     let x = env.clone();
-
-//         FileDatabase {
-//             name,
-//             age,
-//             db: makeDB().unwrap(),
-//             //env: envStored,
-//         }
-//     }
-
-//     pub fn add(&mut self, key : String, val: String) -> Result<(), Box<dyn Error>> {
-//         println!("called `db.add()`");
-//         let mut wtxn = self.env.write_txn()?;
-//         self.db.put(&mut wtxn, &key, &val);
-//         wtxn.commit()?;
-
-//         Ok(())
-//     }
-
-//     // pub fn get(&mut self, key : String) -> Result<Option<&str>, Box<dyn Error>> {
-//     //     let mut rtxn = self.env.read_txn()?;
-//     //     let mut res = self.db.get(&mut rtxn, &key);
-//     //     let retVal = res.unwrap()?;
-
-//     //     Ok(retVal);
-//     // }
-// }
-
-pub fn makeNew() -> FileDatabase {
-    println!("called `datastore::new()`");
-
-    let newDb: Database<Str, Str> = makeDB().unwrap();
-
-
-    return  FileDatabase{
-        name: String::from("abc"),
-        age: 123,
-        db: newDb,
-    };
-}
-
-
-pub fn add(db:Database<Str, Str>, key : String, val: String) -> Result<(), Box<dyn Error>> {
-    println!("called `datastore::add()`");
-    let env_path = Path::new("target").join("test2.mdb");
-
-    fs::create_dir_all(&env_path)?;
-    let env = EnvOpenOptions::new().open(env_path)?;
-    let mut wtxn = env.write_txn()?;
-    db.put(&mut wtxn, &key, &val);
-    wtxn.commit()?;
-    Ok(())
-}
-
-pub fn get(db:Database<Str, Str>, key : String) -> Result<String, Box<dyn Error>> {
-    println!("called `datastore::get()`");
-    let env_path = Path::new("target").join("test2.mdb");
-
-    fs::create_dir_all(&env_path)?;
-    let env = EnvOpenOptions::new().open(env_path)?;
-    let mut rtxn = env.read_txn()?;
-    let res = db.get(&rtxn, &key)?;
-    //let retVal = res.or_else("none");
-
-    Ok(String::from("abc"))
-}
-
-pub fn makeDB() -> Result<Database<Str, Str>, Box<dyn Error>> {
-    
-    let env_path = Path::new("target").join("test2.mdb");
+    let env_path = Path::new("target").join(name.to_owned()+".mdb");
     let _ = fs::remove_dir_all(&env_path);
-    fs::create_dir_all(&env_path)?;
-    let env = EnvOpenOptions::new()
+    fs::create_dir_all(&env_path);
+    let env= EnvOpenOptions::new()
         .map_size(10 * 1024 * 1024) // 10MB
         .max_dbs(3)
-        .open(env_path)?;
+        .open(env_path).unwrap();
+
+    let db = makeDB(&env).unwrap();
+
+        FileDatabase {
+            name,
+            age,
+            environment: env,
+            db: db,
+            //env: envStored,
+        }
+    }
+
+    fn makeDB(env: Env) -> Result<Database<Str, Str>, Box<dyn Error>> {
+    
+        
+        println!("called `makeDB`");
+        let first: Database<Str, Str> = env.create_database( Some("first"))?;
+        println!("called `makeDB`");
+    
+    
+        Ok(first)
+    }
+
+    pub fn add(&mut self, key : String, val: String) -> Result<(), Box<dyn Error>> {
+        println!("called `db.add()`");
+        let mut wtxn = self.environment.write_txn()?;
+        self.db.put(&mut wtxn, &key, &val);
+        wtxn.commit()?;
+
+        Ok(())
+    }
+
+    pub fn get(&mut self, key : String) -> Result<Option<&str>, Box<dyn Error>> {
+        let mut finalVal:Option<&str> = Some("xyz");
+        let mut wtxn = self.environment.write_txn()?;
+        {
+            let get_result = self.db.get(&mut wtxn, &key)?;
+            println!("get_result:\t{:?}", get_result);
+            //finalVal = get_result.clone();
+        }
+        wtxn.commit();
+        Ok(finalVal)
+
+        // match finalVal {
+        //     Some() =>
+        //     None() =>
+        // }
+
+        // match get_result {
+        //     Ok(val) => Ok(val),
+        //     Err(e) => Err(e.into()),
+        // }
+    }
+}
+
+// pub fn makeNew() -> FileDatabase {
+//     println!("called `datastore::new()`");
+
+//     let newDb: Database<Str, Str> = makeDB().unwrap();
+
+
+//     return  FileDatabase{
+//         name: String::from("abc"),
+//         age: 123,
+//         db: newDb,
+//     };
+// }
+
+
+// pub fn add(db:Database<Str, Str>, key : String, val: String) -> Result<(), Box<dyn Error>> {
+//     println!("called `datastore::add()`");
+//     let env_path = Path::new("target").join("test2.mdb");
+
+//     fs::create_dir_all(&env_path)?;
+//     let env = EnvOpenOptions::new().open(env_path)?;
+//     let mut wtxn = env.write_txn()?;
+//     db.put(&mut wtxn, &key, &val);
+//     wtxn.commit()?;
+//     Ok(())
+// }
+
+// pub fn get(db:Database<Str, Str>, key : String) -> Result<String, Box<dyn Error>> {
+//     println!("called `datastore::get()`");
+//     let env_path = Path::new("target").join("test2.mdb");
+
+//     fs::create_dir_all(&env_path)?;
+//     let env = EnvOpenOptions::new().open(env_path)?;
+//     let mut rtxn = env.read_txn()?;
+//     let res = db.get(&rtxn, &key)?;
+//     //let retVal = res.or_else("none");
+
+//     Ok(String::from("abc"))
+// }
+
+fn makeDB(env:&Env) -> Result<Database<Str, Str>, Box<dyn Error>> {
+    
     println!("called `makeDB`");
     let first: Database<Str, Str> = env.create_database( Some("first"))?;
     println!("called `makeDB`");
--- _RSpace_plusPlus/main.rs
+++ _RSpace_plusPlus/main.rs
@@ -1,15 +1,36 @@
+use crate::datastore::FileDatabase;
+
 mod datastore;
 fn main() {
     println!("hello world!");
-    //let mut db = datastore::makeNew();//(String::from("abc"),123);
+    //let mut d0datastore::makeNew();//(String::from("abc"),123);
 
+    let mut db :FileDatabase = FileDatabase::new(String::from("test db"), 123);
 
-    let db = datastore::makeNew();
-    println!("{}", db.name);
-    println!("{}", db.age);
-    datastore::add(db.db,String::from("key"), String::from("val"));
-    datastore::add(db.db,String::from("key1"), String::from("val1"));
-    datastore::add(db.db,String::from("key2"), String::from("val2"));
+    db.add(String::from("abc"), String::from("123"));
+    db.add(String::from("def"), String::from("321"));
+    db.add(String::from("xyz"), String::from("333"));
+
+    {
+        let result1 = db.get(String::from("abc")).unwrap();
+        println!("result1: {:?}", result1);
+    }
+    {
+        let result2 = db.get(String::from("def")).unwrap();
+        println!("result2: {:?}", result2);
+    }
+    {
+        let result3 = db.get(String::from("xyz")).unwrap();
+        println!("result3: {:?}", result3);
+    }
+    
+
+    // let db = datastore::makeNew();
+    // println!("{}", db.name);
+    // println!("{}", db.age);
+    // datastore::add(db.3,String::from("key"), String::from("val"));
+    // datastore::add(db.3,String::from("key1"), String::from("val1"));
+    // datastore::add(db.3,String::from("key2"), String::from("val2"));
 
     // let res = db.add(String::from("key"), String::from("val")).unwrap();
     // //let res2 = db.add(String::from(db.name), String::from(db.age.to_string())).unwrap();
