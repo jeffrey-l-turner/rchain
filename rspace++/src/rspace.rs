f66c640deaa7eab2dcd0399c7b80d65ca44339ea
Stephen Preston <stevepreston747@gmail.com>
2023-04-17 14:14:15 -0700

    add 16 verb functions to rspace struct

--- rspace++/src/rspace.rs
+++ rspace++/src/rspace.rs
@@ -1,12 +1,16 @@
-use std::error::Error;
+use crate::diskconc::DiskConcDB;
 use crate::diskseq::DiskSeqDB;
 use crate::memconc::MemConcDB;
 use crate::memseq::MemSeqDB;
+use crate::shared::{OptionResult, Pattern};
+use std::error::Error;
 
+// See https://docs.google.com/document/d/1yWdvJwsq4Ft7elzKBM0dehh4RFoQ-vXt-1TAUTLLxMY/edit
 pub struct RSpace<D, K> {
-    diskseq: DiskSeqDB<D,K>,
-    memseq: MemSeqDB<D,K>,
-    memconc: MemConcDB<D,K>,
+    diskseq: DiskSeqDB<D, K>,
+    diskconc: DiskConcDB<D, K>,
+    memseq: MemSeqDB<D, K>,
+    memconc: MemConcDB<D, K>,
 }
 
 impl<
@@ -24,20 +28,164 @@ impl<
     > RSpace<D, K>
 {
     pub fn create() -> Result<RSpace<D, K>, Box<dyn Error>> {
-        
         let ds = DiskSeqDB::create().unwrap();
+        let dc = DiskConcDB::create().unwrap();
         let ms = MemSeqDB::create().unwrap();
         let mc = MemConcDB::create().unwrap();
 
         Ok(RSpace {
             diskseq: ds,
+            diskconc: dc,
             memseq: ms,
             memconc: mc,
         })
     }
 
-    pub fn process_command() {
-
+    // Verb Set 1
+    pub fn get_once_durable_concurrent(
+        &self,
+        channel: &str,
+        entry: D,
+    ) -> Option<OptionResult<D, K>> {
+        return self.diskconc.produce(channel, entry, false);
     }
 
+    pub fn get_once_non_durable_concurrent(
+        &self,
+        channel: &str,
+        entry: D,
+    ) -> Option<OptionResult<D, K>> {
+        return self.memconc.produce(channel, entry, false);
+    }
+
+    pub fn get_once_durable_sequential(
+        &self,
+        channel: &str,
+        entry: D,
+    ) -> Option<OptionResult<D, K>> {
+        return self.diskseq.produce(channel, entry, false);
+    }
+
+    pub fn get_once_non_durable_sequential(
+        &self,
+        channel: &str,
+        entry: D,
+    ) -> Option<OptionResult<D, K>> {
+        return self.memseq.produce(channel, entry, false);
+    }
+
+    // Verb Set 2
+    pub fn get_always_durable_concurrent(
+        &self,
+        channel: &str,
+        entry: D,
+    ) -> Option<OptionResult<D, K>> {
+        return self.diskconc.produce(channel, entry, true);
+    }
+
+    pub fn get_always_non_durable_concurrent(
+        &self,
+        channel: &str,
+        entry: D,
+    ) -> Option<OptionResult<D, K>> {
+        return self.memconc.produce(channel, entry, true);
+    }
+
+    pub fn get_always_durable_sequential(
+        &self,
+        channel: &str,
+        entry: D,
+    ) -> Option<OptionResult<D, K>> {
+        return self.diskseq.produce(channel, entry, true);
+    }
+
+    pub fn get_always_non_durable_sequential(
+        &self,
+        channel: &str,
+        entry: D,
+    ) -> Option<OptionResult<D, K>> {
+        return self.memseq.produce(channel, entry, true);
+    }
+
+    // Verb Set 3
+    pub fn put_once_durable_concurrent(
+        &self,
+        channels: Vec<&str>,
+        patterns: Vec<Pattern<D>>,
+        continuation: K,
+    ) -> Option<Vec<OptionResult<D, K>>> {
+        return self
+            .diskconc
+            .consume(channels, patterns, continuation, false);
+    }
+
+    pub fn put_once_non_durable_concurrent(
+        &self,
+        channels: Vec<&str>,
+        patterns: Vec<Pattern<D>>,
+        continuation: K,
+    ) -> Option<Vec<OptionResult<D, K>>> {
+        return self
+            .memconc
+            .consume(channels, patterns, continuation, false);
+    }
+
+    pub fn put_once_durable_sequential(
+        &self,
+        channels: Vec<&str>,
+        patterns: Vec<Pattern<D>>,
+        continuation: K,
+    ) -> Option<Vec<OptionResult<D, K>>> {
+        return self
+            .diskseq
+            .consume(channels, patterns, continuation, false);
+    }
+
+    pub fn put_once_non_durable_sequential(
+        &self,
+        channels: Vec<&str>,
+        patterns: Vec<Pattern<D>>,
+        continuation: K,
+    ) -> Option<Vec<OptionResult<D, K>>> {
+        return self.memseq.consume(channels, patterns, continuation, false);
+    }
+
+    // Verb Set 4
+    pub fn put_always_durable_concurrent(
+        &self,
+        channels: Vec<&str>,
+        patterns: Vec<Pattern<D>>,
+        continuation: K,
+    ) -> Option<Vec<OptionResult<D, K>>> {
+        return self
+            .diskconc
+            .consume(channels, patterns, continuation, true);
+    }
+
+    pub fn put_always_non_durable_concurrent(
+        &self,
+        channels: Vec<&str>,
+        patterns: Vec<Pattern<D>>,
+        continuation: K,
+    ) -> Option<Vec<OptionResult<D, K>>> {
+        return self.memconc.consume(channels, patterns, continuation, true);
+    }
+
+    pub fn put_always_durable_sequential(
+        &self,
+        channels: Vec<&str>,
+        patterns: Vec<Pattern<D>>,
+        continuation: K,
+    ) -> Option<Vec<OptionResult<D, K>>> {
+        return self.diskseq.consume(channels, patterns, continuation, true);
+    }
+
+    pub fn put_always_non_durable_sequential(
+        &self,
+        channels: Vec<&str>,
+        patterns: Vec<Pattern<D>>,
+        continuation: K,
+    ) -> Option<Vec<OptionResult<D, K>>> {
+        return self.memseq.consume(channels, patterns, continuation, true);
+    }
 }
