0d3b88ce01ee693a6c19db9d09bf6cccc2568fef
Stephen Preston <stevepreston747@gmail.com>
2023-04-18 17:54:30 -0700

    add slightly more complex rust example for jna

--- rspace++/src/lib.rs
+++ rspace++/src/lib.rs
@@ -6,17 +6,64 @@ pub mod memseq;
 pub mod rspace;
 pub mod shared;
 
+use std::error::Error;
+
+use example::{Address, Entry, Name, Printer};
 use rspace::RSpace;
+// use std::error::Error;
 
 // #[no_mangle]
-pub extern "C" fn get_rspace<
-    D: Clone + std::hash::Hash + std::fmt::Debug + serde::Serialize + for<'a> serde::Deserialize<'a>,
-    K: Clone
-        + std::hash::Hash
-        + std::fmt::Debug
-        + serde::Serialize
-        + for<'a> serde::Deserialize<'a>
-        + 'static,
->() -> RSpace<D, K> {
-    return RSpace::create().unwrap();
+// #[link(name = "my_library_name")]
+// pub fn create<
+//     D: Clone + std::hash::Hash + std::fmt::Debug + serde::Serialize + for<'a> serde::Deserialize<'a>,
+//     K: Clone
+//         + std::hash::Hash
+//         + std::fmt::Debug
+//         + serde::Serialize
+//         + for<'a> serde::Deserialize<'a>
+//         + 'static,
+// >() -> Result<RSpace<D, K>, Box<dyn Error>> {
+//     return RSpace::create();
+// }
+
+// #[no_mangle]
+// pub extern "C" fn add(x: i32, y: i32) -> i32 {
+//     x + y
+// }
+
+// #[no_mangle]
+// pub extern "C" fn create() -> RSpace<i32, i32> {
+//     let rspace: RSpace<i32, i32> = RSpace::create().unwrap();
+//     return rspace;
+// }
+
+// Example 1
+// #[repr(C)]
+// pub struct MyStruct {
+//     pub a: i32,
+//     pub b: i32,
+// }
+
+// impl MyStruct {
+//     pub fn add(&self) -> i32 {
+//         self.a + self.b
+//     }
+// }
+
+// #[no_mangle]
+// pub extern "C" fn create_my_struct(a: i32, b: i32) -> *mut MyStruct {
+//     Box::into_raw(Box::new(MyStruct { a, b }))
+// }
+
+// Example 2
+pub fn print_types_inner<T: std::fmt::Debug, U: std::fmt::Debug>(x: T, y: U) {
+    println!("Type of x: {:?}", std::any::type_name::<T>());
+    println!("Value of x: {:?}", x);
+    println!("Type of y: {:?}", std::any::type_name::<U>());
+    println!("Value of y: {:?}", y);
+}
+
+#[no_mangle]
+pub extern "C" fn print_types(x: i32, y: f64) {
+    print_types_inner(x, y);
 }
--- rspace++/src/main/scala/Main.scala
+++ rspace++/src/main/scala/Main.scala
@@ -4,16 +4,20 @@ object BuildRustLibrary extends Library {
   // Load the Rust library. The name of the library file may differ depending on your platform.
   val _ = System.setProperty("jna.library.path", "./rspace++/target/release")
 
+  // Define interface for Rust functions
   trait RustLib extends Library {
-    def get_once_durable_concurrent(): Unit
+    // def create(): Unit
+    def create_my_struct(a: Int, b: Int): Pointer
+    def print_types(x: Int, y: Double): Unit
   }
 
   def main(args: Array[String]): Unit = {
     // Load the Rust library
-    val rspace = Native.load("rspace_plus_plus", classOf[RustLib]).asInstanceOf[RustLib]
+    val lib = Native.load("rspace_plus_plus", classOf[RustLib]).asInstanceOf[RustLib]
 
-    // Call the hello_world function
-    rspace.get_once_durable_concurrent();
+    // rspace.create();
+    lib.print_types(42, 3.14);
+
   }
 }
 
