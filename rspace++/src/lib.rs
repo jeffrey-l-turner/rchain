f280e2db8327de2fc7aa1d30220da10aab5559db
Stephen Preston <stevepreston747@gmail.com>
2023-04-19 16:19:55 -0700

    rust to scala using struct

--- rspace++/src/lib.rs
+++ rspace++/src/lib.rs
@@ -6,54 +6,23 @@ pub mod memseq;
 pub mod rspace;
 pub mod shared;
 
-use std::error::Error;
-
-use example::{Address, Entry, Name, Printer};
-use rspace::RSpace;
-// use std::error::Error;
-
-// #[no_mangle]
-// #[link(name = "my_library_name")]
-// pub fn create<
-//     D: Clone + std::hash::Hash + std::fmt::Debug + serde::Serialize + for<'a> serde::Deserialize<'a>,
-//     K: Clone
-//         + std::hash::Hash
-//         + std::fmt::Debug
-//         + serde::Serialize
-//         + for<'a> serde::Deserialize<'a>
-//         + 'static,
-// >() -> Result<RSpace<D, K>, Box<dyn Error>> {
-//     return RSpace::create();
-// }
-
-// #[no_mangle]
-// pub extern "C" fn add(x: i32, y: i32) -> i32 {
-//     x + y
-// }
-
-// #[no_mangle]
-// pub extern "C" fn create() -> RSpace<i32, i32> {
-//     let rspace: RSpace<i32, i32> = RSpace::create().unwrap();
-//     return rspace;
-// }
-
 // Example 1
-// #[repr(C)]
-// pub struct MyStruct {
-//     pub a: i32,
-//     pub b: i32,
-// }
+#[repr(C)]
+pub struct MyStruct {
+    x: i32,
+    y: i32,
+}
 
-// impl MyStruct {
-//     pub fn add(&self) -> i32 {
-//         self.a + self.b
-//     }
-// }
+#[no_mangle]
+pub extern "C" fn MyStruct_new(x: i32, y: i32) -> *mut MyStruct {
+    Box::into_raw(Box::new(MyStruct { x, y }))
+}
 
-// #[no_mangle]
-// pub extern "C" fn create_my_struct(a: i32, b: i32) -> *mut MyStruct {
-//     Box::into_raw(Box::new(MyStruct { a, b }))
-// }
+#[no_mangle]
+pub extern "C" fn MyStruct_add(my_struct: *const MyStruct) -> i32 {
+    let my_struct = unsafe { &*my_struct };
+    my_struct.x + my_struct.y
+}
 
 // Example 2
 pub fn print_types_inner<T: std::fmt::Debug, U: std::fmt::Debug>(x: T, y: U) {
--- rspace++/src/main/scala/Main.scala
+++ rspace++/src/main/scala/Main.scala
@@ -6,18 +6,22 @@ object BuildRustLibrary extends Library {
 
   // Define interface for Rust functions
   trait RustLib extends Library {
-    // def create(): Unit
     def create_my_struct(a: Int, b: Int): Pointer
     def print_types(x: Int, y: Double): Unit
+
+    def MyStruct_new(x: Int, y: Int): Long
+    def MyStruct_add(myStructPtr: Long): Int
   }
 
   def main(args: Array[String]): Unit = {
     // Load the Rust library
     val lib = Native.load("rspace_plus_plus", classOf[RustLib]).asInstanceOf[RustLib]
 
-    // rspace.create();
-    lib.print_types(42, 3.14);
+    // lib.print_types(42, 3.14);
+    val myStructPtr = lib.MyStruct_new(1, 2)
+    val result      = lib.MyStruct_add(myStructPtr)
 
+    println(result)
   }
 }
 
