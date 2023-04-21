a1a9ad6c82fe33420f6524cf620588a2e37e2701
Stephen Preston <stevepreston747@gmail.com>
2023-04-21 12:00:21 -0700

    add all get functions to lib.rs

--- rspace++/src/lib.rs
+++ rspace++/src/lib.rs
@@ -22,6 +22,7 @@ pub extern "C" fn space_new() -> *mut Space<String, String> {
     }))
 }
 
+// Verb Set 1
 #[no_mangle]
 pub extern "C" fn space_get_once_durable_concurrent(
     rspace: *mut Space<String, String>,
@@ -39,6 +40,135 @@ pub extern "C" fn space_get_once_durable_concurrent(
         Box::into_raw(Box::new(result))
     }
 }
+
+#[no_mangle]
+pub extern "C" fn space_get_once_non_durable_concurrent(
+    rspace: *mut Space<String, String>,
+    channel: *const c_char,
+    entry: *const c_char,
+) -> *mut Option<OptionResult<String, String>> {
+    unsafe {
+        let channel_str = CStr::from_ptr(channel).to_str().unwrap();
+        let entry_str = { CStr::from_ptr(entry) }.to_string_lossy().into_owned();
+
+        let result = (*rspace)
+            .rspace
+            .get_once_non_durable_concurrent(channel_str, entry_str);
+
+        Box::into_raw(Box::new(result))
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn space_get_once_durable_sequential(
+    rspace: *mut Space<String, String>,
+    channel: *const c_char,
+    entry: *const c_char,
+) -> *mut Option<OptionResult<String, String>> {
+    unsafe {
+        let channel_str = CStr::from_ptr(channel).to_str().unwrap();
+        let entry_str = { CStr::from_ptr(entry) }.to_string_lossy().into_owned();
+
+        let result = (*rspace)
+            .rspace
+            .get_once_durable_sequential(channel_str, entry_str);
+
+        Box::into_raw(Box::new(result))
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn space_get_once_non_durable_sequential(
+    rspace: *mut Space<String, String>,
+    channel: *const c_char,
+    entry: *const c_char,
+) -> *mut Option<OptionResult<String, String>> {
+    unsafe {
+        let channel_str = CStr::from_ptr(channel).to_str().unwrap();
+        let entry_str = { CStr::from_ptr(entry) }.to_string_lossy().into_owned();
+
+        let result = (*rspace)
+            .rspace
+            .get_once_non_durable_sequential(channel_str, entry_str);
+
+        Box::into_raw(Box::new(result))
+    }
+}
+
+// Verb Set 2
+#[no_mangle]
+pub extern "C" fn space_get_always_durable_concurrent(
+    rspace: *mut Space<String, String>,
+    channel: *const c_char,
+    entry: *const c_char,
+) -> *mut Option<OptionResult<String, String>> {
+    unsafe {
+        let channel_str = CStr::from_ptr(channel).to_str().unwrap();
+        let entry_str = { CStr::from_ptr(entry) }.to_string_lossy().into_owned();
+
+        let result = (*rspace)
+            .rspace
+            .get_always_durable_concurrent(channel_str, entry_str);
+
+        Box::into_raw(Box::new(result))
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn space_get_always_non_durable_concurrent(
+    rspace: *mut Space<String, String>,
+    channel: *const c_char,
+    entry: *const c_char,
+) -> *mut Option<OptionResult<String, String>> {
+    unsafe {
+        let channel_str = CStr::from_ptr(channel).to_str().unwrap();
+        let entry_str = { CStr::from_ptr(entry) }.to_string_lossy().into_owned();
+
+        let result = (*rspace)
+            .rspace
+            .get_always_non_durable_concurrent(channel_str, entry_str);
+
+        Box::into_raw(Box::new(result))
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn space_get_always_durable_sequential(
+    rspace: *mut Space<String, String>,
+    channel: *const c_char,
+    entry: *const c_char,
+) -> *mut Option<OptionResult<String, String>> {
+    unsafe {
+        let channel_str = CStr::from_ptr(channel).to_str().unwrap();
+        let entry_str = { CStr::from_ptr(entry) }.to_string_lossy().into_owned();
+
+        let result = (*rspace)
+            .rspace
+            .get_always_durable_sequential(channel_str, entry_str);
+
+        Box::into_raw(Box::new(result))
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn space_get_always_non_durable_sequential(
+    rspace: *mut Space<String, String>,
+    channel: *const c_char,
+    entry: *const c_char,
+) -> *mut Option<OptionResult<String, String>> {
+    unsafe {
+        let channel_str = CStr::from_ptr(channel).to_str().unwrap();
+        let entry_str = { CStr::from_ptr(entry) }.to_string_lossy().into_owned();
+
+        let result = (*rspace)
+            .rspace
+            .get_always_non_durable_sequential(channel_str, entry_str);
+
+        Box::into_raw(Box::new(result))
+    }
+}
+
+// Verb Set 3
 
 #[no_mangle]
 pub extern "C" fn space_print(rspace: *mut Space<String, String>, channel: *const c_char) -> () {
