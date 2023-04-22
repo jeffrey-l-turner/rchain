99e83fae725c3daebb7a7cbc58947d41c9f086eb
Stephen Preston <stevepreston747@gmail.com>
2023-04-21 17:57:16 -0700

    put function wip

--- rspace++/src/lib.rs
+++ rspace++/src/lib.rs
@@ -7,7 +7,7 @@ pub mod rspace;
 pub mod shared;
 
 use rspace::RSpace;
-use shared::OptionResult;
+use shared::{OptionResult, Pattern};
 use std::ffi::{c_char, CStr};
 
 #[repr(C)]
@@ -36,6 +36,34 @@ pub extern "C" fn space_get_once_durable_concurrent(
         let result = (*rspace)
             .rspace
             .get_once_durable_concurrent(channel_str, entry_str);
+
+        Box::into_raw(Box::new(result))
+    }
+}
+
+// Verb Set 3
+#[no_mangle]
+pub extern "C" fn space_put_once_durable_concurrent(
+    rspace: *mut Space<String, String>,
+    channels: *const Vec<c_char>,
+    patterns: *const Vec<Pattern<String>>,
+    continuation: *const c_char,
+) -> *mut Option<Vec<OptionResult<String, String>>> {
+    unsafe {
+        let channels_values = (*channels)
+            .iter()
+            .map(|ptr| CStr::from_ptr(ptr).to_str().unwrap())
+            .collect();
+        let patterns_values = (&*patterns).to_vec();
+        let continuation_value = { CStr::from_ptr(continuation) }
+            .to_string_lossy()
+            .into_owned();
+
+        let result = (*rspace).rspace.put_once_durable_concurrent(
+            channels_values,
+            patterns_values,
+            continuation_value,
+        );
 
         Box::into_raw(Box::new(result))
     }
@@ -168,8 +196,6 @@ pub extern "C" fn space_get_always_non_durable_sequential(
     }
 }
 
-// Verb Set 3
-
 #[no_mangle]
 pub extern "C" fn space_print(rspace: *mut Space<String, String>, channel: *const c_char) -> () {
     unsafe {
@@ -184,3 +210,33 @@ pub extern "C" fn space_clear(rspace: *mut Space<String, String>) -> () {
         (*rspace).rspace.clear_store();
     }
 }
+
+/*
+#[no_mangle]
+pub extern "C" fn space_put_once_durable_concurrent(
+    // raw pointers. c_char represents C string type
+    rspace: *mut Space<c_char, c_char>,
+    channels: *const Vec<c_char>,
+    patterns: *const Vec<Pattern<c_char>>,
+    continuation: *const c_char,
+) -> *mut Option<Vec<OptionResult<c_char, c_char>>> {
+    unsafe {
+        // dereference pointers
+        // TODO: channel should be type String not &str
+        let channels_values: Vec<&str> = (*channels)
+            .iter()
+            .map(|ptr| CStr::from_ptr(ptr).to_str().unwrap())
+            .collect();
+        let patterns_values = (&*patterns).to_vec();
+        let continuation_value = *continuation;
+
+        let result = (*rspace).rspace.put_once_durable_concurrent(
+            channels_values,
+            patterns_values,
+            continuation_value,
+        );
+
+        Box::into_raw(Box::new(result))
+    }
+}
+*/
--- rspace++/src/main/scala/Main.scala
+++ rspace++/src/main/scala/Main.scala
@@ -8,6 +8,8 @@ object BuildRustLibrary extends Library {
 
   // Define interface for Rust functions
   trait RustLib extends Library {
+    trait Pattern[D] extends Function1[D, Boolean]
+
     def space_new(): Pointer
     def space_print(rspace: Pointer, channel: Pointer): Unit
     def space_clear(rspace: Pointer): Unit
@@ -17,16 +19,47 @@ object BuildRustLibrary extends Library {
         channel: Pointer,
         entry: Pointer
     ): Pointer
+
+    def space_put_once_durable_concurrent(
+        rspace: Pointer,
+        channels: Array[Pointer],
+        patterns: List[Pattern[String]],
+        continuation: Pointer
+    ): Pointer
   }
 
   def main(args: Array[String]): Unit = {
     // Load the Rust library
     val lib = Native.load("rspace_plus_plus", classOf[RustLib]).asInstanceOf[RustLib]
 
+    // type Pattern = Function1[String, Boolean]
+
+    trait Pattern[D] extends Function1[D, Boolean]
+
+    // def toRustPattern(f: Pattern): Pointer = {
+    //   val cb = new Callback(f)
+    //   Pointer.fromFunction(cb, Function.C_CONVENTION)
+    // }
+
+    // val nameMatchPtr: Function1[String, Boolean] = (s: String) => {
+    //   s == "alice"
+    // }
+
+    // val patternsVec = Seq[Pattern](
+    //   s => s == "alice"
+    // ).map(toRustPattern).toArray
+
+    val patterns: Vector[Pattern[String]] = Vector(
+      (s: String) => s == "alice"
+    )
+
+    val javaPatterns: List[Pattern[String]] = patterns.asJava
+
     val spacePtr = lib.space_new()
 
-    val channel = "friends"
-    val entry   = "alice"
+    val channel      = "friends"
+    val entry        = "alice"
+    val continuation = "k-function"
 
     val channelBytes  = channel.getBytes(StandardCharsets.UTF_8)
     val channelBuffer = ByteBuffer.allocateDirect(channelBytes.length)
@@ -36,11 +69,26 @@ object BuildRustLibrary extends Library {
     val entryBuffer = ByteBuffer.allocateDirect(entryBytes.length)
     entryBuffer.put(entryBytes)
 
-    val channelPtr: Pointer = Native.getDirectBufferPointer(channelBuffer)
-    val entryPtr: Pointer   = Native.getDirectBufferPointer(entryBuffer)
+    val continuationBytes  = continuation.getBytes(StandardCharsets.UTF_8)
+    val continuationBuffer = ByteBuffer.allocateDirect(continuationBytes.length)
+    continuationBuffer.put(continuationBytes)
 
-    val res1 = lib.space_get_once_durable_concurrent(spacePtr, channelPtr, entryPtr)
-    println("Result 1: ", res1)
+    val channelPtr: Pointer      = Native.getDirectBufferPointer(channelBuffer)
+    val entryPtr: Pointer        = Native.getDirectBufferPointer(entryBuffer)
+    val continuationPtr: Pointer = Native.getDirectBufferPointer(continuationBuffer)
+
+    // val patternsPtr = new Memory(NativeLong.SIZE * patternsVec.length)
+    // patternsPtr.write(0, patternsVec, 0, patternsVec.length)
+
+    lib.space_put_once_durable_concurrent(
+      spacePtr,
+      Array(channelPtr),
+      javaPatterns,
+      continuationPtr
+    )
+
+    lib.space_get_once_durable_concurrent(spacePtr, channelPtr, entryPtr)
+    // println("Result 1: ", res1)
 
     lib.space_print(spacePtr, channelPtr)
 
