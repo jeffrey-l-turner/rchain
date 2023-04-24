7b9fb272c633c8cf2872fb45f2a4d39815f37312
steve preston <stevepreston747@gmail.com>
2023-04-23 18:15:59 -0700

    use one Main class, package

--- rspace++/src/main/scala/example.scala
+++ rspace++/src/main/scala/example.scala
@@ -2,5 +2,5 @@ import rspacePlusPlus.RustLibrary
 
 object Main {
   def main(args: Array[String]): Unit =
-    RustLibrary.helloWorld()
+    RustLibrary.run()
 }
--- rspace++/src/main/scala/package.scala
+++ rspace++/src/main/scala/package.scala
@@ -1,18 +1,62 @@
 package rspacePlusPlus
 
 import com.sun.jna._
+import java.nio.charset.StandardCharsets
+import java.nio.ByteBuffer
 
 object RustLibrary extends Library {
-  // Load the Rust library. The name of the library file may differ depending on your platform.
   val _ = System.setProperty("jna.library.path", "./rspace++/target/release")
 
   trait RustLib extends Library {
-    def hello_world(): Unit
+    trait Pattern[D] extends Function1[D, Boolean]
+
+    def space_new(): Pointer
+    def space_print(rspace: Pointer, channel: Pointer): Unit
+    def space_clear(rspace: Pointer): Unit
+
+    def space_get_once_durable_concurrent(
+        rspace: Pointer,
+        channel: Pointer,
+        entry: Pointer
+    ): Pointer
+
+    def space_put_once_durable_concurrent(
+        rspace: Pointer,
+        channels: Array[Pointer],
+        patterns: List[Pattern[String]],
+        continuation: Pointer
+    ): Pointer
   }
 
-  // Load the Rust library and call the hello_world function
-  def helloWorld(): Unit = {
-    val lib = Native.load("rust_library", classOf[RustLib]).asInstanceOf[RustLib]
-    lib.hello_world()
+  val lib = Native.load("rspace_plus_plus", classOf[RustLib]).asInstanceOf[RustLib]
+
+  def run(): Unit = {
+    val spacePtr = lib.space_new()
+
+    val channel      = "friends"
+    val entry        = "alice"
+    val continuation = "k-function"
+
+    val channelBytes  = channel.getBytes(StandardCharsets.UTF_8)
+    val channelBuffer = ByteBuffer.allocateDirect(channelBytes.length)
+    channelBuffer.put(channelBytes)
+
+    val entryBytes  = entry.getBytes(StandardCharsets.UTF_8)
+    val entryBuffer = ByteBuffer.allocateDirect(entryBytes.length)
+    entryBuffer.put(entryBytes)
+
+    val continuationBytes  = continuation.getBytes(StandardCharsets.UTF_8)
+    val continuationBuffer = ByteBuffer.allocateDirect(continuationBytes.length)
+    continuationBuffer.put(continuationBytes)
+
+    val channelPtr: Pointer = Native.getDirectBufferPointer(channelBuffer)
+    // val entryPtr: Pointer   = Native.getDirectBufferPointer(entryBuffer)
+
+    // lib.space_get_once_durable_concurrent(spacePtr, channelPtr, entryPtr)
+
+    lib.space_print(spacePtr, channelPtr)
+
+    lib.space_clear(spacePtr)
   }
+
 }
