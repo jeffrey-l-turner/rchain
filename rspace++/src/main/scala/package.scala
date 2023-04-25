662da6534d0df1c2c3b1272f2cfccb2cbeebfdba
steve preston <stevepreston747@gmail.com>
2023-04-24 19:59:01 -0700

    use type string instead of pointer for entry and channel

--- rspace++/src/main/scala/package.scala
+++ rspace++/src/main/scala/package.scala
@@ -11,20 +11,20 @@ object RustLibrary extends Library {
     trait Pattern[D] extends Function1[D, Boolean]
 
     def space_new(): Pointer
-    def space_print(rspace: Pointer, channel: Pointer): Unit
+    def space_print(rspace: Pointer, channel: String): Unit
     def space_clear(rspace: Pointer): Unit
 
     def space_get_once_durable_concurrent(
         rspace: Pointer,
-        channel: Pointer,
-        entry: Pointer
+        channel: String,
+        entry: String
     ): Pointer
 
     def space_put_once_durable_concurrent(
         rspace: Pointer,
-        channels: Array[Pointer],
+        channels: Array[String],
         patterns: List[Pattern[String]],
-        continuation: Pointer
+        continuation: String
     ): Pointer
   }
 
@@ -33,28 +33,13 @@ object RustLibrary extends Library {
   def run(): Unit = {
     val spacePtr = lib.space_new()
 
-    val channel      = "friends"
-    val entry        = "alice"
-    val continuation = "k-function"
+    val channel = "friends"
+    val entry   = "alice"
+    // val continuation = "k-function"
 
-    val channelBytes  = channel.getBytes(StandardCharsets.UTF_8)
-    val channelBuffer = ByteBuffer.allocateDirect(channelBytes.length)
-    channelBuffer.put(channelBytes)
+    lib.space_get_once_durable_concurrent(spacePtr, channel, entry)
 
-    val entryBytes  = entry.getBytes(StandardCharsets.UTF_8)
-    val entryBuffer = ByteBuffer.allocateDirect(entryBytes.length)
-    entryBuffer.put(entryBytes)
-
-    val continuationBytes  = continuation.getBytes(StandardCharsets.UTF_8)
-    val continuationBuffer = ByteBuffer.allocateDirect(continuationBytes.length)
-    continuationBuffer.put(continuationBytes)
-
-    val channelPtr: Pointer = Native.getDirectBufferPointer(channelBuffer)
-    // val entryPtr: Pointer   = Native.getDirectBufferPointer(entryBuffer)
-
-    // lib.space_get_once_durable_concurrent(spacePtr, channelPtr, entryPtr)
-
-    lib.space_print(spacePtr, channelPtr)
+    lib.space_print(spacePtr, channel)
 
     lib.space_clear(spacePtr)
   }
